856120988fecb8e2ef5a275b23677872b7389641
Inderpal <inderpalsingh6763@gmail.com>
2021-10-30 20:12:26 -0400

    Added the code for displayAddPage & processAddPage

--- controllers/book.js
+++ controllers/book.js
@@ -2,76 +2,60 @@
 let Book = require('../models/book');
 
 // Gets all books from the Database and renders the page to list all books.
-module.exports.bookList = function(req, res, next) {  
-    Book.find((err, bookList) => {
-        // console.log(bookList);
-        if(err)
-        {
-            return console.error(err);
-        }
-        else
-        {
-            res.render('book/list', {
-                title: 'Book List', 
-                books: bookList
-            })            
-        }
-    });
-}
+module.exports.bookList = function (req, res, next) {
+  Book.find((err, bookList) => {
+    // console.log(bookList);
+    if (err) {
+      return console.error(err);
+    } else {
+      res.render('book/list', {
+        title: 'Book List',
+        books: bookList,
+      });
+    }
+  });
+};
 
 // Gets a book by id and renders the details page.
 module.exports.details = (req, res, next) => {
-    
-    let id = req.params.id;
+  let id = req.params.id;
 
-    Book.findById(id, (err, bookToShow) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        else
-        {
-            //show the edit view
-            res.render('book/details', {
-                title: 'Book Details', 
-                book: bookToShow
-            })
-        }
-    });
-}
+  Book.findById(id, (err, bookToShow) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      //show the edit view
+      res.render('book/details', {
+        title: 'Book Details',
+        book: bookToShow,
+      });
+    }
+  });
+};
 
 // Renders the Add form using the add_edit.ejs template
 module.exports.displayAddPage = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE        
-
-}
+  res.render('book/add_edit', { title: 'Add a Book', book: '' });
+};
 
 // Processes the data submitted from the Add form to create a new book
 module.exports.processAddPage = (req, res, next) => {
-
-    // ADD YOUR CODE HERE
-
-}
+  const newBook = new Book(req.body);
+  Book.create(newBook);
+  res.redirect('/book/list');
+};
 
 // Gets a book by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE
-
-}
+};
 
 // Processes the data submitted from the Edit form to update a book
 module.exports.processEditPage = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE
-    
-}
+  // ADD YOUR CODE HERE
+};
 
 // Deletes a book based on its id.
 module.exports.performDelete = (req, res, next) => {
-    
-    // ADD YOUR CODE HERE
-
-}
+  // ADD YOUR CODE HERE
+};
